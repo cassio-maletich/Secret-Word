b7fc51af2ab14b867968cae0bdd2efa92db808eb
Cassio Maletich <cassio@fullstacklabs.co>
2025-05-25 14:51:48 -0300

    Aula 1

---
+++ src/components/StartScreen.js
@@ -0,0 +1,11 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return <div className='start'>
+    <h1>Secret Word</h1>
+    <p>Click no botão abaixo pra começar</p>
+    <button>Começar o jogo</button>
+  </div>
+}
+
+export default StartScreen
