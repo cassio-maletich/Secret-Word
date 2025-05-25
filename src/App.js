9f61d16a3cb1dd02d86faf616fbccf47c706b8fa
Cassio Maletich <cassio@fullstacklabs.co>
2025-05-25 15:00:17 -0300

    Setup components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
-import StartScreen from './components/StartScreen';
+import './App.css'
+
+import { useCallback, useEffect, useState } from "react"
+
+import { wordsList } from './data/words'
+
+// components
+import StartScreen from './components/StartScreen'
+import { GameOver } from './components/GameOver'
+import { Game } from './components/Game'
+
+const stages = [
+  {id: 1, name: 'start'},
+  {id: 2, name: 'game'},
+  {id: 3, name: 'end'}
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  console.log(words)
+
   return (
     <div className="App">
-      <StartScreen />
+     {gameStage === 'start' && <StartScreen />}
+     {gameStage === 'game' && <Game />}
+     {gameStage === 'end' && <GameOver />}
     </div>
   );
 }
